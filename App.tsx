06abe915c2f7be2014d4f582e5b5c16420558359
Tuyen Le <tuyen.le106@hcmut.edu.vn>
2026-03-27 15:02:29 +0700

    [dev] Config Redux Toolkit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
-// import { store } from "./src/store";
+import { store } from "./src/store";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Navigation from "./src/navigation";
 import "./src/utils/i18n";
-// import colors from "./src/utils/constants";
 
 export default function App() {
   return (
-    // <Provider store={store}>
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Navigation />
-      {/* <StatusBar style="light" backgroundColor={colors.background} /> */}
-    </GestureHandlerRootView>
-    // </Provider>
+    <Provider store={store}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Navigation />
+      </GestureHandlerRootView>
+    </Provider>
   );
 }
